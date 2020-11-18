cc2815097b2c5a11fcafffdb6ef82944ec26f220
wolfy <shitwolfymakes@gmail.com>
2020-11-17 20:03:19 -0700

    moved boilerplate into config.py

---
+++ bot/config.py
@@ -0,0 +1,29 @@
+"""Stores functionality common to the bot"""
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+
+def create_api():
+    # store environment variables
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    # auth to twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # create api object
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
